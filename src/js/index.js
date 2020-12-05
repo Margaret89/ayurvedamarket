4cc651bf374d43ee4537e858b5eccd5c3f4a40cd
Margaret <match14@yandex.ru>
2020-12-05 17:41:40 +0300

    top-slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,20 @@
 import {$} from './common';
 
-$(window).scroll(function(){
-	if($(this).scrollTop()>300){
-		$('.js-move-up').addClass('visible');
-	}else{
-		$('.js-move-up').removeClass('visible');
-	}
+// $(window).scroll(function(){
+// 	if($(this).scrollTop()>300){
+// 		$('.js-move-up').addClass('visible');
+// 	}else{
+// 		$('.js-move-up').removeClass('visible');
+// 	}
+// });
+// $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+
+// top-slider
+$('.js-top-slider').slick({
+	infinite: true,
+	dots: false,
+	arrows: false,
+	autoplay: true,
+	fade: true,
+	cssEase: 'linear'
 });
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
