15d66a124cd7ac984340e90d75e06808b29dc545
Margaret <match14@yandex.ru>
2020-12-06 11:27:38 +0300

    add arrow up

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
 import {$} from './common';
 
-// $(window).scroll(function(){
-// 	if($(this).scrollTop()>300){
-// 		$('.js-move-up').addClass('visible');
-// 	}else{
-// 		$('.js-move-up').removeClass('visible');
-// 	}
-// });
-// $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+// move up
+$(window).on('scroll', function(){
+	if($(this).scrollTop()>300){
+		$('.js-move-up').addClass('visible');
+	}else{
+		$('.js-move-up').removeClass('visible');
+	}
+});
+$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
 
 // top-slider
 $('.js-top-slider').slick({
