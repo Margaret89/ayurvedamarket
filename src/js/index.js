f6535a7d717d10253b3b49fb26127811d5c54ac2
Margaret <match14@yandex.ru>
2020-12-06 20:35:32 +0300

    catalog-list

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import {$} from './common';
 
-// move up
+// Стрелка перемещения вверх
 $(window).on('scroll', function(){
 	if($(this).scrollTop()>300){
 		$('.js-move-up').addClass('visible');
@@ -10,12 +10,45 @@ $(window).on('scroll', function(){
 });
 $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
 
-// top-slider
-$('.js-top-slider').slick({
-	infinite: true,
-	dots: false,
-	arrows: false,
-	autoplay: true,
-	fade: true,
-	cssEase: 'linear'
-});
+// Верхний слайдер
+if($('.js-top-slider').length){
+	$('.js-top-slider').slick({
+		infinite: true,
+		dots: false,
+		arrows: false,
+		autoplay: true,
+		fade: true,
+		cssEase: 'linear'
+	});
+}
+
+// Слайдер категорий
+if($('.js-cat-slider').length){
+	$('.js-cat-slider').slick({
+		infinite: true,
+		dots: false,
+		slidesToShow: 5,
+		slidesToScroll: 5
+	});
+}
+
+// Слайдер товаров
+if($('.js-slider-product').length){
+	$('.js-slider-product').slick({
+		infinite: false,
+		dots: false,
+		slidesToShow: 4,
+		slidesToScroll: 4
+	});
+}
+
+// Свернуть/развернуть текст каталога
+if($('.js-cat-desc-switch').length){
+	$('.js-cat-desc-switch').on('click', function(){
+		var tempVal = $(this).text();
+
+		$(this).text($(this).attr('data-text'));
+		$(this).attr('data-text', tempVal);
+		$('.js-cat-desc-detail').slideToggle(300);
+	});
+}
